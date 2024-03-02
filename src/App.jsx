a34e33db4e7dfcbbc4604ee0cf42ecccaa053926
tanish111 <tanishdesai37@gmail.com>
2024-03-03 01:56:40 +0530

    Burger Menu Basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
-
+import './index.css'
+import BurgerMenu from './components/BurgerMenu'
 function App() {
 
   return (
+    <>
+    <BurgerMenu />
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    </>
   )
 }
 
